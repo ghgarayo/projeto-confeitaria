a6a3cc2835e677c5771f9375041e3cab1ad12aa9
Gustavo Garayo <ghgarayo@gmail.com>
2023-10-02 12:00:38 -0300

    chore: criada rota para registrar usuários

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,36 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/customers', async (request, reply) => {
+  console.log(request.body)
+
+  const registerBodySchema = z.object({
+    name: z.string().min(2).max(50),
+    cpf: z.string().length(11),
+    date_of_birth: z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(6).max(50),
+    phone: z.string().length(11),
+  })
+
+  const { name, cpf, date_of_birth, email, password, phone } =
+    registerBodySchema.parse(request.body)
+
+  const password_hash = password
+
+  await prisma.customer.create({
+    data: {
+      name,
+      cpf,
+      date_of_birth,
+      email,
+      password_hash,
+      phone,
+    },
+  })
+
+  return reply.status(201).send()
+})
