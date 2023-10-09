7092def403312f6b81aed300ffb73196b7258044
Gustavo Garayo <gustavo.garayo@kron.digital>
2023-10-09 20:48:18 -0300

    :fire: removendo arquivos não utilizados

--- server/src/repositories/interfaces/logins-repository.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma, Login } from '@prisma/client'
-
-export interface LoginsRepository {
-  create(data: Prisma.LoginUncheckedCreateInput): Promise<Login>
-  findByCustomerId(customerId: string): Promise<Login | null>
-  findByEmployeeId(employeeId: string): Promise<Login | null>
-}
